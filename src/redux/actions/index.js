336c0997779b9a1ea53d576c490ef880c0106d0a
Lucas Zunino <zunino10@hotmail.com>
2023-02-02 17:03:48 -0300

    Redux CheckList DONE

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const FIRST_TASK = 'FIRST_TASK';
+
+export const firstTask = (task) => ({
+  type: FIRST_TASK,
+  task,
+});
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { FIRST_TASK } from '../actions';
+
+const INITIAL_STATE = {
+  email: '', // string que armazena o email da pessoa usuária
+};
+
+export const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case FIRST_TASK:
+    return state;
+  default:
+    return state;
+  }
+};
